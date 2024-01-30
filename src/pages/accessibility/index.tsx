48fba12a699fc40879dfa03add89d08afbd2ca99
TheTeaCup <lw0482352@gmail.com>
2024-01-30 11:55:07 -0500

    accessibility page setup

---
+++ src/pages/accessibility/index.tsx
@@ -0,0 +1,21 @@
+import SEO from "@/components/seo";
+import { Flex, Text } from "@chakra-ui/react";
+import Navbar from "@/components/navbar";
+
+const AccessibilityHome = () => {
+    return (
+        <>
+            <SEO title={"Accessibility"} />
+
+            <Navbar>
+                <Flex minH={"40vh"} align={"center"} justify={"center"}>
+                    <Text fontSize={{ md: "xl" }}>
+                        Accessibility at AppalCART
+                    </Text>
+                </Flex>
+            </Navbar>
+        </>
+    );
+};
+
+export default AccessibilityHome;
