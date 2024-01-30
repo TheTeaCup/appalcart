9f92b5e50fa9869b3757ce522c3f7632c0b3a7db
TheTeaCup <lw0482352@gmail.com>
2024-01-30 11:32:13 -0500

    contact us page

---
+++ src/pages/contact-us.tsx
@@ -0,0 +1,61 @@
+import SEO from "@/components/seo";
+import { Flex, Text } from "@chakra-ui/react";
+import Navbar from "@/components/navbar";
+
+const Home = () => {
+    return (
+        <>
+            <SEO title={"Home"} />
+
+            <Navbar>
+                {/*
+          <Center>
+          <Grid templateColumns="repeat(5, 1fr)" gap={4}>
+            <GridItem colSpan={2}>
+              <Flex direction="row">
+                <Text
+                  fontFamily={"heading"}
+                  fontSize={[
+                    "2xl", // base
+                    "3xl", // 480px upwards
+                    "6xl", // 1200px upwards
+                  ]}
+                  as={"i"}
+                  color={"red"}
+                >
+                  Appal
+                </Text>
+                <Text
+                  fontFamily={"heading"}
+                  fontSize={[
+                    "2xl", // base
+                    "3xl", // 480px upwards
+                    "6xl", // 1200px upwards
+                  ]}
+                  color={"green"}
+                  marginLeft={0}
+                >
+                  CART
+                </Text>
+              </Flex>
+              <Text noOfLines={[1, 2]}>
+                Providing Public Transportation Since 1981
+              </Text>
+            </GridItem>
+            <GridItem colStart={4} colEnd={6}>
+            </GridItem>
+          </Grid>
+        </Center>
+          */}
+
+                <Flex minH={"40vh"} align={"center"} justify={"center"}>
+                    <Text fontSize={{ md: "xl" }}>
+                        Contact Us Page
+                    </Text>
+                </Flex>
+            </Navbar>
+        </>
+    );
+};
+
+export default Home;
