3520760cef3fd671ee9df103b256c61a7817ce45
TheTeaCup <lw0482352@gmail.com>
2024-01-29 19:07:15 -0500

    chakra ui setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { ColorModeScript } from "@chakra-ui/react";
+import { Head, Html, Main, NextScript } from "next/document";
+import theme from "@/utils/theme";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+const Document = () => {
+    return (
+        <Html lang={"en"}>
+            <Head />
+            <body>
+            <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+            <Main />
+            <NextScript />
+            </body>
+        </Html>
+    );
+};
+
+export default Document;
