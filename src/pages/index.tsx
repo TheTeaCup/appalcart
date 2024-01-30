207266f6014e72edef0d554e49081074c8ce8c27
TheTeaCup <lw0482352@gmail.com>
2024-01-30 18:45:48 -0500

    home page updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,26 @@
 import SEO from "@/components/seo";
-import { Flex, Text } from "@chakra-ui/react";
 import Navbar from "@/components/navbar";
+import {
+  Container,
+  Stack,
+  Flex,
+  Box,
+  Heading,
+  Text,
+  Button,
+  Image,
+  Icon,
+  IconButton,
+  createIcon,
+  IconProps,
+  useColorModeValue,
+  AspectRatio,
+  VStack,
+} from "@chakra-ui/react";
+import { TbBus } from "react-icons/tb";
+import { CiApple } from "react-icons/ci";
+import { FaAppleAlt } from "react-icons/fa";
+import NextLink from "next/link";
 
 const Home = () => {
   return (
@@ -8,51 +28,105 @@ const Home = () => {
       <SEO title={"Home"} />
 
       <Navbar>
-        {/*
-          <Center>
-          <Grid templateColumns="repeat(5, 1fr)" gap={4}>
-            <GridItem colSpan={2}>
-              <Flex direction="row">
-                <Text
-                  fontFamily={"heading"}
-                  fontSize={[
-                    "2xl", // base
-                    "3xl", // 480px upwards
-                    "6xl", // 1200px upwards
-                  ]}
-                  as={"i"}
-                  color={"red"}
-                >
+        <Container maxW={"7xl"}>
+          <Stack
+            align={"center"}
+            spacing={{ base: 8, md: 10 }}
+            py={{ base: 20, md: 28 }}
+            direction={{ base: "column", md: "row" }}
+          >
+            <Stack flex={1} spacing={{ base: 5, md: 10 }}>
+              <Heading
+                lineHeight={1.1}
+                fontWeight={600}
+                fontSize={{ base: "3xl", sm: "4xl", lg: "6xl" }}
+              >
+                <Text as={"i"} position={"relative"} color={"red.400"}>
                   Appal
                 </Text>
-                <Text
-                  fontFamily={"heading"}
-                  fontSize={[
-                    "2xl", // base
-                    "3xl", // 480px upwards
-                    "6xl", // 1200px upwards
-                  ]}
-                  color={"green"}
-                  marginLeft={0}
-                >
+                <Text as={"span"} color={"green.400"}>
                   CART
                 </Text>
-              </Flex>
-              <Text noOfLines={[1, 2]}>
-                Providing Public Transportation Since 1981
+              </Heading>
+              <Text>
+                Providing public transportation for the High Country since 1981
               </Text>
-            </GridItem>
-            <GridItem colStart={4} colEnd={6}>
-            </GridItem>
-          </Grid>
-        </Center>
-          */}
+              <Stack
+                spacing={{ base: 4, sm: 6 }}
+                direction={{ base: "column", sm: "row" }}
+              >
+                <Button
+                  as={NextLink}
+                  href={"/services/paratransit"}
+                  variant={"ghost"}
+                  rounded={"full"}
+                  size={"lg"}
+                  fontWeight={"normal"}
+                  px={6}
+                >
+                  Paratransit
+                </Button>
+                <Button
+                  as={NextLink}
+                  href={"/route-maps-and-schedules"}
+                  variant={"ghost"}
+                  rounded={"full"}
+                  size={"lg"}
+                  fontWeight={"normal"}
+                  px={6}
+                >
+                  Fixed-Routes
+                </Button>
+                <Button
+                  as={NextLink}
+                  href={"/route-maps-and-schedules"}
+                  variant={"ghost"}
+                  rounded={"full"}
+                  size={"lg"}
+                  fontWeight={"normal"}
+                  px={6}
+                >
+                  Night Owl
+                </Button>
+              </Stack>
+            </Stack>
+            <Flex
+              flex={1}
+              justify={"center"}
+              align={"center"}
+              position={"relative"}
+              w={"full"}
+            >
+              <Box
+                position={"relative"}
+                height={"full"}
+                rounded={"2xl"}
+                boxShadow={"2xl"}
+                width={"full"}
+                overflow={"hidden"}
+              >
+                <AspectRatio>
+                  <iframe
+                    src="https://www.youtube.com/embed/9LeVgqGI40Y?list=TLGGw3D4ubzv15QzMDAxMjAyNA"
+                    title="YouTube video player"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                  />
+                </AspectRatio>
 
-        <Flex minH={"40vh"} align={"center"} justify={"center"}>
-          <Text fontSize={{ md: "xl" }}>
-            Hi, this site is still under construction. Please check back later.
-          </Text>
-        </Flex>
+                {/*<Image
+                              alt={"YouTube Video"}
+                              fit={"cover"}
+                              align={"center"}
+                              w={"100%"}
+                              h={"100%"}
+                              src={"https://youtu.be/9LeVgqGI40Y?list=TLGGw3D4ubzv15QzMDAxMjAyNA"}
+                          />
+*/}
+              </Box>
+            </Flex>
+          </Stack>
+        </Container>
       </Navbar>
     </>
   );
