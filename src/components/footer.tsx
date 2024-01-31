46ec812f95a92b2724521f971f1b0d80d40ceefe
TheTeaCup <lw0482352@gmail.com>
2024-01-30 23:17:55 -0500

    footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,45 +1,10 @@
 import {
     Box,
-    chakra,
     Container, Flex, Heading,
     Stack,
     Text,
     useColorModeValue,
-    VisuallyHidden,
 } from "@chakra-ui/react"
-import { FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa"
-import { ReactNode } from "react"
-
-const SocialButton = ({
-                          children,
-                          label,
-                          href,
-                      }: {
-    children: ReactNode
-    label: string
-    href: string
-}) => {
-    return (
-        <chakra.button
-            bg={useColorModeValue("blackAlpha.100", "whiteAlpha.100")}
-            rounded={"full"}
-            w={8}
-            h={8}
-            cursor={"pointer"}
-            as={"a"}
-            href={href}
-            display={"inline-flex"}
-            alignItems={"center"}
-            justifyContent={"center"}
-            transition={"background 0.3s ease"}
-            _hover={{
-                bg: useColorModeValue("blackAlpha.200", "whiteAlpha.200"),
-            }}>
-            <VisuallyHidden>{label}</VisuallyHidden>
-            {children}
-        </chakra.button>
-    )
-}
 
 const Footer = () => {
     const year = new Date().getFullYear();
