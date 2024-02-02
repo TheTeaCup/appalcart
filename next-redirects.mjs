bdec6d9efef8be3b54754be1b4285c4acf5084f6
TheTeaCup <lw0482352@gmail.com>
2024-02-02 17:09:27 -0500

    build error fix

--- next-redirects.mjs
+++ next-redirects.mjs
@@ -24,6 +24,7 @@ const nextRedirects = {
       {
         source: "/instagram",
         destination: "https://www.instagram.com/appalcarttransit",
+        permanent: true,
       },
     ];
   },
