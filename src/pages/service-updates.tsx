dae9b0702b561ca7961eb7cc282f9be6b80a444e
TheTeaCup <lw0482352@gmail.com>
2024-02-02 16:54:32 -0500

    services updates page

--- src/pages/service-updates.tsx
+++ src/pages/service-updates.tsx
@@ -3,19 +3,17 @@ import { Flex, Text } from "@chakra-ui/react";
 import Navbar from "@/components/navbar";
 
 const Home = () => {
-    return (
-        <>
-            <SEO title={"Service Updates"} />
+  return (
+    <>
+      <SEO title={"Service Updates"} />
 
-            <Navbar>
-                <Flex minH={"40vh"} align={"center"} justify={"center"}>
-                    <Text fontSize={{ md: "xl" }}>
-                        Service Updates for AppalCART
-                    </Text>
-                </Flex>
-            </Navbar>
-        </>
-    );
+      <Navbar>
+        <Flex minH={"40vh"} align={"center"} justify={"center"}>
+          <Text fontSize={{ md: "xl" }}>Service Updates for AppalCART</Text>
+        </Flex>
+      </Navbar>
+    </>
+  );
 };
 
 export default Home;
