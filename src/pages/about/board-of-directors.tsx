0261d6ee57d855e0da578ac9f61449ee26a1ca8b
TheTeaCup <lw0482352@gmail.com>
2024-02-03 17:20:35 -0500

    board of directors

--- src/pages/about/board-of-directors.tsx
+++ src/pages/about/board-of-directors.tsx
@@ -1,6 +1,8 @@
 import SEO from "@/components/seo";
-import { Flex, Text } from "@chakra-ui/react";
+import {Avatar, Center, chakra, Container, Divider, Flex, Heading, SimpleGrid, Text, VStack} from "@chakra-ui/react";
 import Navbar from "@/components/navbar";
+import BoardOfDirectors from "@/data/board_of_directors.json";
+
 
 const AboutBoardOfDirectors = () => {
   return (
@@ -8,9 +10,68 @@ const AboutBoardOfDirectors = () => {
       <SEO title={"Board of Directors"} />
 
       <Navbar>
-        <Flex minH={"40vh"} align={"center"} justify={"center"}>
-          <Text fontSize={{ md: "xl" }}>Board of Directors</Text>
-        </Flex>
+          <Container maxWidth="7xl" p={{ base: 2, sm: 10 }}>
+              <VStack spacing={8} align="center">
+                  <Heading fontSize="4xl" fontWeight="bold">
+                      Board of Directors
+                  </Heading>
+                  <Text textAlign="center" fontSize="lg">
+                      The AppalCART Board oversees the operation of the Paratransit and Fixed Route systems in Watauga County, North Carolina.
+                      Eight members comprise the board, and members of the AppalCART Board must be appointed by the Watauga County Commissioners.
+                      All members must reside within the territorial jurisdiction of AppalCART.
+                      The AppalCART Board has a wide variety of members representing various interests and occupations in the community.
+                  </Text>
+              </VStack>
+
+              <Center>
+                  <Divider
+                      css={{ borderBottomWidth: "3px" }}
+                      size={"10px"}
+                      width={"50%"}
+                      mt={5}
+                      mb={5}
+                  />
+              </Center>
+
+              <Center mt={5}>
+                  <SimpleGrid
+                      columns={[1, 2]}
+                      spacing={20}
+                      justifyContent="center"
+                      justifyItems="center"
+                      placeContent={"center"}
+                      mb={5}
+                  >
+              {BoardOfDirectors.map((member) => (
+                  <div key={member.id}>
+                        <VStack spacing={4} align="center">
+                            {/*<chakra.img*/}
+                            {/*    src={member.image}*/}
+                            {/*    alt={member.name}*/}
+                            {/*    borderRadius="full"*/}
+                            {/*    boxSize="150px"*/}
+                            {/*    objectFit="cover"*/}
+                            {/*/>*/}
+                            <Avatar
+                                bg={"green.400"}
+                                color={"white"}
+                                src={member.image}
+                                name={member.name}
+                                size="2xl"
+                            />
+                            <Text fontSize="xl" fontWeight="bold">
+                                {member.name}
+                            </Text>
+                            <Text mt={-5} fontSize="lg">
+                                {member.title}
+                            </Text>
+                        </VStack>
+                  </div>
+                  ))}
+                  </SimpleGrid>
+              </Center>
+
+          </Container>
       </Navbar>
     </>
   );
